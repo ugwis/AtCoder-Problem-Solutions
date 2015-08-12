9b4b125d03e51007133cc24361b5f01c0d47ef05
ugwis <ugwis@icloud.com>
2015-08-13 01:06:59 +0900

    AOJ2015 was more simple solution

--- AOJ/Volume 20/2015.cpp
+++ AOJ/Volume 20/2015.cpp
@@ -7,29 +7,27 @@ using namespace std;
 int main(){
 	int n, m;
 	while (cin >> n >> m, n){
-		vector<int> w, h;
-		int ws = 0, hs = 0;
-		h.push_back(0);
+		vector<int> h(n), w(m);
+		for (auto &i : h) cin >> i;
+		for (auto &i : w) cin >> i;
+		vector<int> hl(n*1000), wl(m*1000);
 		for (int i = 0; i < n; i++){
-			int hi;
-			cin >> hi;
-			h.push_back(hs += hi);
+			int sum = 0;
+			for (int j = i; j < n; j++){
+				sum += h[j];
+				hl[sum]++;
+			}
 		}
-		w.push_back(0);
 		for (int i = 0; i < m; i++){
-			int wi;
-			cin >> wi;
-			w.push_back(ws += wi);
-		}
-		vector<int> diag(ws + hs + 1);
-		for (int unsigned i = 0; i < h.size(); i++){
-			for (int unsigned j = 0; j < w.size(); j++){
-				diag[h[i] + w[j]]++;
+			int sum = 0;
+			for (int j = i; j < m; j++){
+				sum += w[j];
+				wl[sum]++;
 			}
 		}
 		int ret = 0;
-		for (int unsigned i = 0; i < diag.size(); i++){
-			ret += diag[i]*(diag[i]-1)/2;
+		for (int i = 0; i < min(n, m)*1000; i++){
+			ret += hl[i] * wl[i];
 		}
 		cout << ret << endl;
 	}
