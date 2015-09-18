ccdc59ee33ed2f8fb59bbd6d15ec3b8d95f3cdba
Yuta K <ugwis@icloud.com>
2015-09-18 23:46:35 +0900

    CODE FES 予選A C問題

---
+++ AtCoder/Contest/C.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+ 
+using namespace std;
+ 
+int main(){
+	int a, b;
+	cin >> a >> b;
+	int ret = b / 4;
+	ret -= (a - 1) / 4;
+	for (int i = 0; i <= 2000000000; i+=100){
+		if (a <= i && b >= i) ret--;
+	}
+	for (int i = 0; i <= 2000000000; i += 400){
+		if (a <= i && b >= i) ret++;
+	}
+	cout << ret << endl;
+	return 0;
+}
