7f8b84543ef1e59cc7d83b47ca7815e4cae626bd
Yuta K <ugwis@icloud.com>
2015-09-28 22:26:10 +0900

    CODE FES QUALA C 解法を変えました

--- AtCoder/Contest/code-festival-2014-quala/C.cpp
+++ AtCoder/Contest/code-festival-2014-quala/C.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
- 
+
 using namespace std;
- 
+
+int countLY(int y) {
+	int ret = y / 4;
+	ret -= y/100;
+	ret += y/400;
+	return ret;
+}
+
 int main(){
 	int a, b;
 	cin >> a >> b;
-	int ret = b / 4;
-	ret -= (a - 1) / 4;
-	for (int i = 0; i <= 2000000000; i+=100){
-		if (a <= i && b >= i) ret--;
-	}
-	for (int i = 0; i <= 2000000000; i += 400){
-		if (a <= i && b >= i) ret++;
-	}
-	cout << ret << endl;
+	cout << (countLY(b) - countLY(a-1)) << endl;
 	return 0;
 }
