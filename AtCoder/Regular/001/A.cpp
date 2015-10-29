bbcf6cb091fc410308048822872a5c9d8770a8d8
ugwis <orn_tk@me.com>
2015-10-30 08:54:49 +0900

    ARC001 A

---
+++ AtCoder/Regular/001/A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int cnt[4];
+ 
+int main(){
+	int n;
+	cin >> n;
+	for(int i=0;i<n;i++){
+		char c;
+		cin >> c;
+		cnt[c-'1']++;
+	}
+	int mini=100,maxi=0;
+	for(int i=0;i<4;i++){
+		mini = min(mini,cnt[i]);
+		maxi = max(maxi,cnt[i]);
+	}
+	cout << maxi << " " << mini << endl;
+	return 0;
+}
