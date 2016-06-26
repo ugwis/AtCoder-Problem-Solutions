2b25a55f12864a25cba774e2bbe24f4403b80294
Kitamura Yuta <ugwis@icloud.com>
2016-06-26 20:58:06 +0900

    add code_festival_relay_b

---
+++ AtCoder/Contest/code-festival-2014-relay/code_festival_relay_b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int a,s;
+    cin >> a >> s;
+    cout << (a>=s?"Congratulations!":"Enjoy another semester...") << endl;
+    return 0;
+}
