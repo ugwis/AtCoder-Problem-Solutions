5010cda7cf31339b2883add2fa6f723b1d436081
Kitamura Yuta <ugwis@icloud.com>
2016-06-26 20:58:00 +0900

    add arc003_2

---
+++ AtCoder/Regular/003/arc003_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	vector<string> s;
+	for(int i=0;i<n;i++){
+		string str;
+		cin >> str;
+		reverse(str.begin(),str.end());
+		s.emplace_back(str);
+	}
+	sort(s.begin(),s.end());
+	for(int i=0;i<n;i++){
+		reverse(s[i].begin(),s[i].end());
+		cout << s[i] << endl;
+	}
+	return 0;
+}
