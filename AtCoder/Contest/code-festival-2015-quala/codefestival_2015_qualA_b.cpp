f3530743a6bb7e39824fe2d4b95c18d97186cba5
Kitamura Yuta <ugwis@icloud.com>
2016-06-26 20:57:55 +0900

    add codefestival_2015_qualA_b

---
+++ AtCoder/Contest/code-festival-2015-quala/codefestival_2015_qualA_b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+        int n,ans=0;
+        cin >> n;        
+        for(int i=0;i<n;i++){
+                int a;
+                cin >> a;
+                ans+=a*pow(2,n-i-1);
+        }
+        cout << ans << endl;
+        return 0; 
+}
