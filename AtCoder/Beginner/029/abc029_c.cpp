83929212f1f6fddf4f1195987d3e61162ba20985
Kitamura Yuta <ugwis@icloud.com>
2016-06-26 20:58:11 +0900

    add abc029_c

--- AtCoder/Beginner/029/abc029_c.cpp
+++ AtCoder/Beginner/029/abc029_c.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-void rec(int n,string s){
-	if(!n) cout << s << endl;
-	else for(char i='a';i<='c';i++){
-		rec(n-1,s+i);
-	}
-}
-
 int main(){
-	int n; cin >> n;
-	rec(n,"");
+	int n;
+	cin >> n;
+	string s;
+	for(int i=0;i<n;i++){
+		s += 'a';
+	}
+	bool escape = false;
+	while(!escape){
+		cout << s << endl;
+		s[n-1]++;
+		while(s.find("d",0) != string::npos){
+			int r = (int)s.find("d",0);
+			if(r - 1 < 0){
+				escape = true;
+				break;
+			}
+			s[r - 1]++;
+			s[r] = 'a';
+		}
+	}
+
+	//	cout << ret << endl;
 	return 0;
 }
