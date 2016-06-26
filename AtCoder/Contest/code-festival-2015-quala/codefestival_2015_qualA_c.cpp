75485ae7d6592c9df9df01cc8ea84a7d4d780dd1
Kitamura Yuta <ugwis@icloud.com>
2016-06-26 20:57:55 +0900

    add codefestival_2015_qualA_c

---
+++ AtCoder/Contest/code-festival-2015-quala/codefestival_2015_qualA_c.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+using namespace std;
+
+int main(){
+	int i,n,t,sum=0;
+	vector<int> v;
+	
+	cin >> n >> t;
+	
+	for(int i=0;i<n;i++){
+		int a,b;
+		cin >> a >> b;
+		v.push_back(a-b);
+		sum+=a;
+	}
+	
+	sort(v.begin(),v.end(),greater<int>());
+	
+	for(i=0;i<n;i++){
+		if(sum<=t) break;
+		sum-=v[i];
+	}
+	cout << (sum<=t?i:-1) << endl;
+	return 0;
+}
