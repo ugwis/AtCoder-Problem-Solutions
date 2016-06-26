08f58bba4e228a96e008d56934089e985ec5872e
Kitamura Yuta <ugwis@icloud.com>
2016-06-26 20:58:00 +0900

    add arc003_1

---
+++ AtCoder/Regular/003/arc003_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main(){
+	int n,sum=0;
+	cin >> n;
+	for(int i=0;i<n;i++){
+		char c;
+		cin >> c;
+		sum += 4 - c + 'A' + (c == 'F');
+	}
+	cout << setprecision(20);
+	cout << (double)sum/n << endl;
+	return 0;
+}
