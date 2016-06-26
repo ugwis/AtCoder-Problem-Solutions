232a48f9819d59d2fccd7b65c52236f4efca6ec0
Kitamura Yuta <ugwis@icloud.com>
2016-06-26 20:58:35 +0900

    add arc002_2

---
+++ AtCoder/Regular/002/arc002_2.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+ 
+using namespace std;
+ 
+int ld[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+int main(){
+	int y,m,d;
+	scanf("%d/%d/%d",&y,&m,&d);
+	while(y%(m*d) != 0){
+		if(d == ld[m-1]+(((y%4==0&&y%100!=0)||y%400==0)&&m==2)){
+			m++;
+			d = 0;
+		}
+		if(m > 12) m = 1,y++;
+		d++;
+	}
+	printf("%d/%02d/%02d\n",y,m,d);
+	
+	return 0;
+}
