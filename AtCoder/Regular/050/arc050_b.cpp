41fa85d6c2be5cbd8d9e7f114bbf49754a12c967
Kitamura Yuta <ugwis@icloud.com>
2016-06-26 20:57:56 +0900

    add arc050_b

---
+++ AtCoder/Regular/050/arc050_b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define lli long long int
+using namespace std;
+ 
+int main(){
+	lli R,B,x,y;
+	cin >> R >> B >> x >> y;
+	lli l = 0, r = max(R,B);
+	lli K = (l+r)/2;
+	while(l < r-1){
+		lli k = (R - K)/(x - 1) + (B - K)/(y - 1);
+		if(k >= K && K <= min(R,B)){
+			l = K;
+		} else {
+			r = K;
+		}
+		K = (l+r)/2;
+	}
+	cout << K << endl;
+	
+	return 0;
+}
