4d18519e143a38f907229bcbb10aea29a413ea8a
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:49 +0900

    add abc002_3

--- AtCoder/Beginner/002/abc002_3.cpp
+++ AtCoder/Beginner/002/abc002_3.cpp
@@ -1,22 +1,12 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-	vector<pair<int,int>> v;
-	for(int i=0;i<3;i++){
-		int x,y;
-		cin >> x >> y;
-		v.push_back(make_pair(x,y));
-	}
-	sort(v.begin(),v.end());
-	int dx = -v[0].first;
-	int dy = -v[0].second;
-	for(int i=0;i<3;i++){
-		v[i].first += dx;
-		v[i].second += dy;
-	}
-	cout << setprecision(12);
-	cout << abs(v[1].second*v[2].first - v[1].first*v[2].second)/2.0 << endl;
-
-	return 0;
-}
+        #include <bits/stdc++.h>
+         
+        using namespace std;
+         
+        int main(){
+        	int x[3],y[3];
+        	for(int i=0;i<3;i++) cin >> x[i] >> y[i];
+        	for(int i=1;i<3;i++){x[i]-=x[0];y[i]-=y[0];}
+            cout << setprecision(12);
+        	cout << abs(x[1]*y[2] - y[1]*x[2])/2.0 << endl;
+        	return 0;
+        }
