3bc991d125b3383c96d850bf8d239d7bebc63b8a
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:14 +0900

    add codefestival_2015_qualA_a

--- AtCoder/Contest/code-festival-2015-quala/codefestival_2015_qualA_a.cpp
+++ AtCoder/Contest/code-festival-2015-quala/codefestival_2015_qualA_a.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
 int main(){
-	string s;
-	cin >> s;
-	for(int i=0;i<s.size()-1;i++){
-		cout << s[i];
-	}
-	cout << 5 << endl;
-	return 0;
+        string str;
+        cin >> str;
+        str[str.size()-1]='5';
+        cout << str << endl;
+        return 0;
 }
