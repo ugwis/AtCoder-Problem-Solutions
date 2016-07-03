80aeb759e5e4e681e411148b4433927b2cae4528
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:53 +0900

    add nyc2015_1

--- AtCoder/Contest/nyc2015/nyc2015_1.cpp
+++ AtCoder/Contest/nyc2015/nyc2015_1.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
-#include <bitset>
+#include <string>
 using namespace std;
 
 int main() {
 	int n;
+	string s;
 	cin >> n;
-	//111011100110101100101000000000
-	bitset<32> value(n);
-	//cout << value.to_string() << endl;
-	int it;
-	for(it=(int)value.size()-1;it>=0;it--){
-		if(value[it]!=0) break;
+	while(n){
+		s.push_back((n&1) + '0');
+		n>>=1;
 	}
-	it+=1;
-	//cout << "it = " << it << endl;
-	//cout << "value.size() = " << value.size() << endl;
-	for(int i=0;i<it/2;i++){
-		if(value[i]!=value[it-i-1]){
-			cout << "No" << endl;
-			return 0;
-		}
+	//cout << string(s.rbegin(),s.rend()) << endl;
+	if(s == string(s.rbegin(),s.rend())){
+		cout << "Yes" << endl;
+	} else {
+		cout << "No" << endl;
 	}
-	cout << "Yes" << endl;
 	return 0;
 }
