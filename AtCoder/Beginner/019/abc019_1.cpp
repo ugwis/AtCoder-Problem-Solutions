b4a6264f9f6f1932f3b943ec6aeec72ed710c40a
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:43 +0900

    add abc019_1

--- AtCoder/Beginner/019/abc019_1.cpp
+++ AtCoder/Beginner/019/abc019_1.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int main(){
-    int a[3];
-    cin >> a[0] >> a[1] >> a[2];
-    sort(a,a+3);
-    cout << a[1] <<endl;
-return 0;
+
+int main() {
+	int a[3];
+	for(int i=0;i<3;i++) cin >> a[i];
+	sort(a,a+3);
+	cout << a[1] << endl;
+	return 0;
 }
-    
