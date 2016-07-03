5e4c778062ed4ce206a6d84f06782596e1d24cf7
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:31 +0900

    add chokudai_001_a

--- AtCoder/Contest/chokudai001/chokudai_001_a.cpp
+++ AtCoder/Contest/chokudai001/chokudai_001_a.cpp
@@ -8,81 +8,54 @@ int a[SIZE][SIZE];
 
 int d[5]={0,-1,0,1,0};
 
-vector<pair<int,int>> get_peaks(){
-        vector<pair<int,int>> ret;
-        int maxi = 0;
-        for(int i=0;i<SIZE;i++){
-                for(int j=0;j<SIZE;j++){
-                        if(maxi <= a[i][j]){
-                                if(maxi < a[i][j]) ret.clear();
-                                maxi = a[i][j];
-                                ret.emplace_back(make_pair(i,j));
-                        }
-                }
-        }
-        return ret;
+pair<int,int> get_peak(){
+	pair<int,int> ret(make_pair(0,0));
+	for(int i=0;i<SIZE;i++){
+		for(int j=0;j<SIZE;j++){
+			if(a[ret.first][ret.second] < a[i][j]){
+				ret = make_pair(i,j);
+			}
+		}
+	}
+	return ret;
 }
 
-vector<pair<int,int>> route;
-vector<pair<int,int>> search_route(int x,int y){
-/*      cout << "[" ;
-        for(int unsigned i = 0;i<route.size();i++){
-                cout << "(" << route[i].first << "," << route[i].second << ") ";
-        }
-        cout << "]" << endl;*/
-
-        vector<pair<int,int>> ret;
-        for(int i=0;i<4;i++){
-                int X = x+d[i];
-                int Y = y+d[i+1];
-                if(X < 0 || X >= SIZE) continue;
-                if(Y < 0 || Y >= SIZE) continue;
-                if(a[x][y]-1 != a[X][Y]) continue;
-                if(a[X][Y] <= 0) continue;
-                if(find(route.begin(),route.end(),make_pair(X,Y)) != route.end()) continue;
-                route.push_back(make_pair(X,Y));
-                vector<pair<int,int>> r = search_route(X,Y);
-                if(r.size() > ret.size()){
-                        ret = r;
-                }
-                route.pop_back();
-        }
-        if(ret.empty()) return route;
-        return ret;
+void search_sur(int x,int y){
+	pair<int,int> next(x,y);
+	if(a[x][y] <= 0) return;
+	cout << x+1 << " " << y+1 << endl;
+	a[x][y]--;
+	for(int i=3;i>=0;i--){
+		if(x+d[i] < 0 || x+d[i] >= SIZE) continue;
+		if(y+d[i+1] < 0 || y+d[i+1] >= SIZE) continue;
+		if(a[x][y] == a[x+d[i]][y+d[i+1]]){
+			next = make_pair(x+d[i],y+d[i+1]);
+			break;
+		}
+	}
+	if(next != make_pair(x,y)) search_sur(next.first,next.second);
 }
 
 int main(){
-        for(int i=0;i<SIZE;i++){
-                for(int j=0;j<SIZE;j++){
-                        cin >> a[i][j];
-                }
-        }
-        while(1){
-                auto peaks = get_peaks();
-                vector<pair<int,int>> best_route;
-                for(auto peak:peaks){
-                        route.push_back(peak);
-                        if(a[peak.first][peak.second] <= 0) break;
-                        vector<pair<int,int>> r = search_route(peak.first,peak.second);
-                        if(r.size() > best_route.size()) best_route=r;
-                        route.pop_back();
-                }
-                if(best_route.empty()) break;
-                for(int unsigned i = 0;i<best_route.size();i++){
-                        cout << best_route[i].first+1 << " " << best_route[i].second+1
-<< endl;
-                        a[best_route[i].first][best_route[i].second]--;
-                }
-                /*
-                cout << endl;
-                for(int i=0;i<SIZE;i++){
-                        for(int j=0;j<SIZE;j++){
-                                cout << a[i][j] << " ";
-                        }
-                        cout << endl;
-                }
-                cout << "---" << endl;*/
-
-        }
-        return 0;
+	for(int i=0;i<SIZE;i++){
+		for(int j=0;j<SIZE;j++){
+			cin >> a[i][j];
+		}
+	}
+	while(1){
+		auto x = get_peak();
+		if(a[x.first][x.second] <= 0) break;
+		search_sur(x.first,x.second);
+		/*
+		cout << endl;
+		for(int i=0;i<SIZE;i++){
+			for(int j=0;j<SIZE;j++){
+				cout << a[i][j] << " ";
+			}
+			cout << endl;
+		}
+		cout << "---" << endl;
+		*/
+	}
+	return 0;
 }
