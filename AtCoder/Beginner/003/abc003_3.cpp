007b2daea0004a9a445109eb59f3c692cc329a69
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:47:21 +0900

    add abc003_3

--- AtCoder/Beginner/003/abc003_3.cpp
+++ AtCoder/Beginner/003/abc003_3.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 int main(){
-    int n,k;
-    cin >> n >> k;
-    int r[100];
-    for(int i=0;i<n;i++){
-        cin >> r[i];
-    }
-    sort(r,r + n);
-    long double c = 0;
-    for(int i=n-k;i<n;i++){
-        c = (c + r[i])/2;
-    }
-    cout << setprecision(20);
-    cout << c << endl;
-    return 0;
+	int n,k;
+	cin >> n >> k;
+	int r[101];
+	for(int i=0;i<n;i++){
+		cin >> r[i];
+	}
+	sort(r, r + n);
+	long double rate = 0.0;
+	for(int i=n-k;i<n;i++){
+		rate = (rate + r[i])/2.0;
+	}
+	cout << setprecision(20);
+	cout << rate << endl;
+	return 0;
 }
