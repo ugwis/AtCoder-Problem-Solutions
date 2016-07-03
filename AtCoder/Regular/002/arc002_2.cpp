69c142922575eff7c1b5f23c6ca3cf6b00ca55cd
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:40 +0900

    add arc002_2

--- AtCoder/Regular/002/arc002_2.cpp
+++ AtCoder/Regular/002/arc002_2.cpp
@@ -3,12 +3,16 @@
 using namespace std;
  
 int ld[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
+ 
+bool leap(int y){
+	return (y%4==0&&y%100!=0)||y%400==0;
+}
+ 
 int main(){
 	int y,m,d;
 	scanf("%d/%d/%d",&y,&m,&d);
 	while(y%(m*d) != 0){
-		if(d == ld[m-1]+(((y%4==0&&y%100!=0)||y%400==0)&&m==2)){
+		if(d == ld[m-1]+(leap(y)&&m==2)){
 			m++;
 			d = 0;
 		}
