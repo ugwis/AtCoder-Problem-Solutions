83f127751c4fedc6e8b80841cb1586e3b34721ce
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:17 +0900

    add abc021_a

--- AtCoder/Beginner/021/abc021_a.cpp
+++ AtCoder/Beginner/021/abc021_a.cpp
@@ -1,24 +1,10 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+
 using namespace std;
-
-int d[4] = {1,2,4,8};
-
 int main(){
 int n;
 cin >> n;
-vector<int> v;
-for(int i=3;i>=0;i--){
-while(n>=d[i]){
-n-=d[i];
-v.push_back(d[i]);
-}
-}
-sort(v.begin(),v.end());
-cout << v.size() << endl;
-for(int i=0;i<v.size();i++){
-cout << v[i] << endl;
-}
+cout << n << endl;
+for(int i=0;i<n;i++) cout << 1 << endl;
 return 0;
 }
