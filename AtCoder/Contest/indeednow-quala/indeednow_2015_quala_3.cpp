e83ec0168b3566dd9cff3985c8866a56d97d9a4a
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:23 +0900

    add indeednow_2015_quala_3

--- AtCoder/Contest/indeednow-quala/indeednow_2015_quala_3.cpp
+++ AtCoder/Contest/indeednow-quala/indeednow_2015_quala_3.cpp
@@ -1,33 +1,18 @@
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-	int n;
-	//int count[1000001];
-	int ss[100002];
-	//memset(count,0,sizeof(count));
-	int maxi=0;
-	cin >> n;
+int main(){
+	int n;cin>>n;
+	vector<int> s(n+1,-1);
 	for(int i=0;i<n;i++){
-		int s;
-		cin >> s;
-		if(s==0) ss[i] = -1;
-		else ss[i] = s;
-		maxi = max(maxi,s);
+		cin >> s[i];
+		if(s[i] == 0) s[i]--;
 	}
-	ss[n] = maxi+1;
-	ss[n+1] = -1;
-	sort(ss,ss + n+1,greater<int>());
-	int q;
-	cin >> q;
-	for(int i=0;i<q;i++){
+	sort(s.begin(),s.end(),greater<int>());
+	int q;cin>>q;
+	for (int i = 0; i < q; i++) {
 		int k;
 		cin >> k;
-		cout << ss[k+1]+1 << endl;
+		cout << s[k]+1 << endl;
 	}
 	return 0;
 }
