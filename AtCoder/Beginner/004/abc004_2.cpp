73b8ff9b4484b9ffa1c1f9726a2613780a2ea3eb
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:25 +0900

    add abc004_2

--- AtCoder/Beginner/004/abc004_2.cpp
+++ AtCoder/Beginner/004/abc004_2.cpp
@@ -1,16 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+
 using namespace std;
+
 int main(){
-	char c[4][4];
-	for(int i=0;i<4;i++){
-		for(int j=0;j<4;j++){
-			cin >> c[i][j];
-		}
+	stack<char> c;
+	for(int i=0;i<16;i++){
+		char s;
+		cin >> s;
+		c.push(s);
 	}
 	for(int i=0;i<4;i++){
 		for(int j=0;j<4;j++){
-			if(j!=0) cout << " ";
-			cout << c[3-i][3-j];
+		cout << c.top() << " ";
+		c.pop();
 		}
 		cout << endl;
 	}
