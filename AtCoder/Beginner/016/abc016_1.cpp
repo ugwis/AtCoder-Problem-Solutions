10cde4db4cda8189703fb6fe7d6af52ce57326d4
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:18 +0900

    add abc016_1

--- AtCoder/Beginner/016/abc016_1.cpp
+++ AtCoder/Beginner/016/abc016_1.cpp
@@ -1,11 +1,13 @@
-#include <bits/stdc++.h>
-
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <stack>
 using namespace std;
-
+ 
 int main(){
 	int m,d;
 	cin >> m >> d;
-	if(m%d==0) cout << "YES" << endl;
-	else cout << "NO" << endl;
+	cout << ((m%d==0)?"YES":"NO") << endl;
 	return 0;
 }
