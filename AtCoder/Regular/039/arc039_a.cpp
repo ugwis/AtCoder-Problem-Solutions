6784db9424786896631e9899f0c54b78cdcc2349
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:26 +0900

    add arc039_a

--- AtCoder/Regular/039/arc039_a.cpp
+++ AtCoder/Regular/039/arc039_a.cpp
@@ -1,30 +1,39 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-#include <algorithm>
-#include <typeinfo>
 
 using namespace std;
  
 int main(){
-	int ret=-1000;
-	string a,b;
+	int max=-1000;
+	int a,b;
 	cin >> a >> b;
-	for(char c = '0';c<='9';c++){
-		for(int i=0;i<3;i++){
-			if(i != 0 || c != '0'){
-				char tmp = a[i];
-				a[i] = c;
-				ret = max(ret,stoi(a) - stoi(b));
-				a[i] = tmp;
-	
-				tmp = b[i];
-				b[i] = c;
-				ret = max(ret,stoi(a) - stoi(b));
-				b[i] = tmp;
-			}
+	int aa[3],bb[3];
+	for(int i=0;i<3;i++){
+		aa[i] = a%10;
+		a/=10;
+		bb[i] = b%10;
+		b/=10;
+	}
+	int aaa[4],bbb[4];
+	aaa[0] = aa[2]*100 + aa[1]*10 + aa[0];
+	aaa[1] = 9*100 + aa[1]*10 + aa[0];
+	aaa[2] = aa[2]*100 + 9*10 + aa[0];
+	aaa[3] = aa[2]*100 + aa[1]*10 + 9;
+	bbb[0] = bb[2]*100 + bb[1]*10 + bb[0];
+	bbb[1] = 1*100 + bb[1]*10 + bb[0];
+	bbb[2] = bb[2]*100 + 0*10 + bb[0];
+	bbb[3] = bb[2]*100 + bb[1]*10 + 0;
+	for(int i=0;i<4;i++){
+		if(aaa[i] - bbb[0] > max){
+			max = aaa[i] - bbb[0];
 		}
 	}
-	cout << ret << endl;
+	for(int i=0;i<4;i++){
+		if(aaa[0] - bbb[i] > max){
+			max = aaa[0] - bbb[i];
+		}
+	}
+	cout << max << endl;
 	return 0;
 }
