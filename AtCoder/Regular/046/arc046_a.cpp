a9a95cb3bd414054a605981279041d66bb9e27e5
Kitamura Yuta <ugwis@icloud.com>
2016-07-03 14:46:54 +0900

    add arc046_a

--- AtCoder/Regular/046/arc046_a.cpp
+++ AtCoder/Regular/046/arc046_a.cpp
@@ -1,2 +1,3 @@
-#include<iostream>
-using namespace std;int main(){int n,i;cin>>n;for(i=(n+8)/9;i--;)cout<<(n-1)%9+1;cout<<endl;return 0;}
+#include <iostream>
+using namespace std;
+int main(){int n;cin>>n;for(int i=(n+8)/9;i--;)cout << (n-1)%9+1;cout << endl;return 0;}
