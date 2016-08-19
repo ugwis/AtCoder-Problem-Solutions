b8f40d38d340c945f625df92ef66c7dfd33bdad9
Kitamura Yuta <ugwis@icloud.com>
2016-08-19 19:25:39 +0900

    add abc042_b

---
+++ AtCoder/Beginner/042/abc042_b.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int n,l;
+	cin >> n >> l;
+	vector<string> v(n);
+	for(int i=0;i<n;i++){
+		cin >> v[i];
+	}
+	sort(v.begin(),v.end());
+	for(int unsigned i = 0;i<n;i++){
+		cout << v[i];
+	}
+	cout << endl;
+	
+	return 0;
+}
